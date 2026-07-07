f2015db059cc3206de64c093946b01f3c6e0e4cc
Kuma-ux <mollyjane797@gmail.com>
2026-07-07 17:24:55 +0300

    Update support.js

--- routes/support.js
+++ routes/support.js
@@ -29,12 +29,17 @@ router.post(
 
       const transporter =
         nodemailer.createTransport({
-          service: "gmail",
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
           auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
           },
         });
+
+      await transporter.verify();
+      console.log("SMTP connected");
 
       await transporter.sendMail({
         from: process.env.EMAIL_USER,
